e5fe688cb1b0f02c48b8babafc2bcdfd8d64cd44
Nadav Oved <nadavo@users.noreply.github.com>
2017-04-27 16:37:53 +0300

    fix path to credentials file

--- GMail/Silencio/quickstart.py
+++ GMail/Silencio/quickstart.py
@@ -15,7 +15,7 @@ except ImportError:
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = '../client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 
